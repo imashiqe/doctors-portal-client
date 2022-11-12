193a280dd323b905c8a45e3a7b34dcab374e14ae
imashiqe <imashiqe@gmail.com>
2022-11-13 00:34:48 +0600

    custom color and bg image added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import background  from '../../../assets/images/bg.png';
 const Banner = () => {
     return (
-        <div>
-            <h2>This Banner</h2>
-        </div>
+        <div className="hero  " style={{ backgroundImage: `url(${background})` }}>
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://placeimg.com/260/400/arch" className=" rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  daisyui: {
+     themes:[
+        {
+           doctortheme:{
+               primary: '#0FCFEC',
+               secondary: '#19D3AE',
+               neutral: "#3D4451",
+               accent: "#3A4256",
+               "base-100" : "#FFFFFF"
+           }
+        }
+     ]
+  },
   theme: {
     extend: {},
   },

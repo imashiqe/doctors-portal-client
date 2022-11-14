2dfb829b7387686fb3a44304f6c57b1681953be9
imashiqe <imashiqe@gmail.com>
2022-11-14 11:14:09 +0600

    added react day ficker

--- src/Pages/Appointment/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import background  from '../../../../assets/images/bg.png';
 import chair from '../../../../assets/images/chair.png';
-
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 const AppointmentBanner = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date())
     return (
-        <header>
-   
-          
-            <div className="hero  " style={{ backgroundImage: `url(${background})` }}>
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src={chair} className=" rounded-lg lg:w-1/2 shadow-2xl" />
-    <div>
-     
-    
-  </div>
-</div>
-        </div>
-        
+        <header className='my-6'>
+                 <div className="hero  " style={{ backgroundImage: `url(${background})` }}>
+                 <div className="hero-content flex-col lg:flex-row-reverse">
+                        <img src={chair} className=" rounded-lg lg:w-1/2 shadow-2xl" />
+                 <div className='mr-6'>
+                        <DayPicker
+                          mode='single'
+                          selected={selectedDate}
+                          onSelect={setSelectedDate}
+                        />
+
+                        <p>You Have  Selected Date: {format(selectedDate, 'PP')}</p>
+                 </div>
+                 </div>
+                 </div>
         </header>
     );
 };

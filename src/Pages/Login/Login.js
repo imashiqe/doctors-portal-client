30d6a896f321d0606d8499105d574f091af2cb9e
imashiqe <imashiqe@gmail.com>
2022-11-18 00:06:30 +0600

    react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is Login</h2>
+        <div className='h-[800px] flex justify-center items-center'>
+            <h2 className='text-4xl'>This is Login</h2>
         </div>
     );
 };

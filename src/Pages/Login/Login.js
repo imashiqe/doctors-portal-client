03b78988477846017af59d5d8e7e2ef3ab67e580
imashiqe <imashiqe@gmail.com>
2022-11-18 00:25:17 +0600

    email password field added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState(" ");
     return (
-        <div className='h-[800px] flex justify-center items-center'>
-            <h2 className='text-4xl'>This is Login</h2>
+        <div className='h-[800px]  flex justify-center items-center'>
+            
+            <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+            <h2 className='text-4xl'>Login</h2>
+                <div className="form-control w-full max-w-xs">
+                <label className="label">
+                <span className="label-text">Email</span>
+                
+                </label>
+                <input type="email" placeholder="Type Tour Email" className="input input-bordered w-full max-w-xs"  {...register("email")} />
+
+
+                </div>
+                <div className="form-control w-full max-w-xs">
+                <label className="label">
+                <span className="label-text">Password</span>
+                
+                </label>
+                <input type="password" placeholder="Type Tour Password" className="input input-bordered w-full max-w-xs"  {...register("password")} />
+
+
+                </div>
+
+      <input type="submit" />
+    </form>
         </div>
     );
 };
